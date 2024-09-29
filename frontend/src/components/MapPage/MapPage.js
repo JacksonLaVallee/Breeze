3484b6f3e5292062d1ce3ae447536c7598628772
JacksonLaVallee <jacksonclavallee@gmail.com>
2024-09-29 11:10:51 -0500

    Revert "potential bug fix to zip state"
    This reverts commit d7225c5a6250788505e004a2d8565b6718f966ce.

--- frontend/src/components/MapPage/MapPage.js
+++ frontend/src/components/MapPage/MapPage.js
@@ -9,18 +9,11 @@ function MapPage() {
   const location = useLocation();
   const navigate = useNavigate();
   const places = location.state ? location.state.places : []; // Receive the detailed places data
-  const currentZipCode = location.state ? location.state.zipCode : '60612'; // Use zip code from state or default
 
   return (
     <div>
       <GoogleMapComponent places={places} />
-      <button 
-        onClick={() =>
-          navigate(-1, {
-            state: { zipCode: currentZipCode }, // Pass the current zip code back as state
-          })
-        }
-        className="goBackDateButton">
+      <button onClick={() => navigate(-1)} className="goBackDateButton">
       Choose a Different Date
     </button>
     </div>
