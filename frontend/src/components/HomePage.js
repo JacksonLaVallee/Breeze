9de8f267fcec2c998876124615d4eec643614d32
sidneyamsalem <sidneyamsalem@gmail.com>
2024-09-28 20:47:54 -0500

    updated homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -17,40 +17,17 @@ function HomePage() {
     const currentHour = new Date().getHours();
     let bgUrl = 'https://www.mtnscoop.com/media/images/2020/05/shutterstock_1188687490-1024x535.jpg';
 
-    // TODO
-    // Use weather API to dynamically choose images (if its rainy, then show a rainy background image)
-
+    // TODO: Use weather API to dynamically choose images (if it's rainy, show a rainy background image)
     setBgImage(bgUrl);
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div className="home-page" style={{ textAlign: 'center', marginTop: '50px' }}>
-      {/* Slides or content that you want to display */}
-      <div className="slides">
-        <div className="slide">
-          <img src="file.png" alt="Welcome" />
-          <h2>Welcome to Breeze</h2>
-        </div>
-        <div className="slide">
-          <h2>Never Worry About Weather Again</h2>
-        </div>
-        <div className="slide">
-          <h2>Click Here to Get Started</h2>
-          {/* Add the "Get Started" button */}
-          <button
-            className="get-started-button"
-            onClick={handleGetStarted} // Navigate to date-selection when clicked
-            style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}
-          >
-=======
     <div className="home-page" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="overlay">
         <div className="content">
           <h1 className="title">Welcome to Breeze</h1>
           <p className="subtitle">Discover things to do, no matter the weather.</p>
           <button className="get-started-button" onClick={handleGetStarted}>
->>>>>>> 2b407ef (updated homepage design)
             Get Started
           </button>
         </div>
